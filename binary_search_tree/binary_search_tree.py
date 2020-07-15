26ced22b7a0997cab7f0907e589bb3c03fc92eaa
Adrian Nasaruk <adriannasaruk@gmail.com>
2020-07-15 09:32:57 -1000

    project for today

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,61 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        #case1 value is less than self value
+        if value < self.value:
+            #If there is no left child, insert value here
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                #repeat the process for left subtree
+                self.left.insert(value)
+
+        #case2 value is greater than self value
+        elif value > self.value:
+            #if there is no right child inser value there
+            if self.right is None:
+                self.right = BSTNode(value)
+
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        #case 1 if self.value equals target
+        if self.value == target:
+            return True
+        #case2 if target is less than self.value
+        if target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        #case3 otherwise
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if not self.right:
+            return self.value
+        else:
+            while self.right is not None:
+                if self.right.value > self.value:
+                    self.value = self.right.value
+                    self.right = None
+            return self.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.right is not None:
+            self.right.for_each(fn)
+        if self.left is not None:
+            self.left.for_each(fn)
 
     # Part 2 -----------------------
 
